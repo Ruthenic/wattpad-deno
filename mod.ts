9973c4e37703bac0f2ee0debac46452a202cfe35
Ruthenic <mdrakea3@tutanota.com>
2023-01-15 22:35:41 -0500

    bump ver

--- mod.ts
+++ mod.ts
@@ -4,5 +4,5 @@ export default Wattpad;
 import Chapter from "./src/classes/Chapter.ts";
 import Search from "./src/classes/Search.ts";
 import Story from "./src/classes/Story.ts";
-const VERSION = "v0.0.3";
+const VERSION = "v0.0.4";
 export { Chapter, Search, Story, VERSION, Wattpad };
